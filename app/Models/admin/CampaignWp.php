1675cbb0cfb08773dbf89309be734521b5861774
jmurillo77 <jmurillo77@gmail.com>
2024-11-11 00:01:52 -0500

    as

--- app/Models/admin/CampaignWp.php
+++ app/Models/admin/CampaignWp.php
@@ -10,4 +10,8 @@ class CampaignWp extends Model
     use HasFactory;
     protected $table = 'campaign_wp';
     protected $primaryKey = 'IdCampaignWP';
+
+    public function telefono_movils(){
+        return $this->belongsToMany(TelefonoMovil::class, 'persona_telefono_movils', 'IdPersona', 'IdTelefonoMovil');
+    }
 }
