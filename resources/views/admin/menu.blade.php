96a67338e29978e33de56e3adffcded0ee298d19
jmurillo77 <jmurillo77@gmail.com>
2025-11-27 21:59:18 -0500

    Menu

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -123,6 +123,18 @@
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-success">
                     <div class="inner">
+                        <h3>Cocina</h3>
+                        <p>Cocina</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="{{ route ('admin') }}" class="small-box-footer">Más Info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success">
+                    <div class="inner">
                         <h3>Configuracion</h3>
                         <p>Configuracion</p>
                     </div>
